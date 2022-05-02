e317b580323636043b69d8435a2ab826c489c85f
Sebastian <sebby331@gmail.com>
2022-05-02 17:31:14 -0400

    Create slow.cpp

---
+++ #1/slow.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> sol;
+        for(int i = 0; i < nums.size(); i++){
+            sol.clear();
+            sol.push_back(i);
+            int looking = target - nums[i];
+            for(int j = 0; j < nums.size(); j++){
+                if(i != j and nums[j] == looking){
+                    sol.push_back(j);
+                    return sol;
+                }    
+            }
+        }
+        return sol;
+    }
+};
