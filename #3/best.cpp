77552ee82fb5eae72b65f7f8cb8545b08ba94b5b
Sebastian <sebby331@gmail.com>
2022-05-03 22:23:19 -0400

    Update best.cpp

--- #3/best.cpp
+++ #3/best.cpp
@@ -1,1 +1,23 @@
-//needs to beat O(n^2)
+//beats O(n^2)
+class Solution {
+public:
+    
+    int lengthOfLongestSubstring(string s) {
+        int max = 0;
+        int curr = 0;
+        int left = 0;
+        for(int i = 0; i<s.size(); i++){
+            if(s.substr(left, curr).find(s[i]) != string::npos){
+                int loc = s.substr(left, curr).find(s[i]);
+                left += loc + 1;
+                curr -= loc;   
+            }
+            else{ 
+                curr++;
+                if(curr > max)
+                    max = curr;
+            }
+        } 
+        return max;
+    }
+};
